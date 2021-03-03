0e69baaf32ba45f03b5d3865b58c266e4f94f80c
urbanprivategym <dohyun.kim_93@navercorp.com>
2021-03-03 20:54:39 +0900

    GA 측정값 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-gtag',
       options: {
-        measurementID: '264066890', // 측정 ID
+        measurementID: 'G-264066890', // 측정 ID
         head: false, // head에 tracking script를 넣고 싶다면 true로 변경 
         anonymize: true,
       },
